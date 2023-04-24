25b13a8de888f7dff832db0784e4154ec9647e37
David Josue Rodríguez Chávez <davidjchavez98@gmail.com>
2023-04-24 12:27:10 -0600

    First commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+    import fb_logo from '$lib/images/fb-logo.png';
+</script>
+
+<nav>
+    <img src="{fb_logo}"
+         alt="Fuente de bendiciones"
+         class="fb-logo">
+    <div class="menu-items">
+        <a href="/">Inicio</a>
+        <a href="about">Quienes somos</a>
+        <a>Doctrina</a>
+        <a>Ministerios</a>
+        <a>Eventos</a>
+        <a>Ubicación</a>
+    </div>
+</nav>
+
+<style>
+    nav {
+        background-color: #FFFFFF20;
+        display: flex;
+        align-items: center;
+        border-radius: 1rem;
+        width: 100%;
+    }
+
+    a:link, a:visited {
+        text-decoration: inherit;
+        color: inherit;
+        cursor: pointer;
+    }
+
+    .menu-items {
+        display: flex;
+        gap: 3rem;
+        margin-left: 3rem;
+        color: white;
+    }
+
+    .fb-logo {
+        width: 75px;
+        height: 75px;
+    }
+</style>
