a875dc5a03ac71221e04011f40c7becfec5cf517
David J Chavez <davidjchavez98@gmail.com>
2023-05-24 17:33:05 -0600

    feat(hero): Cambios en pantalla principal, navegación agregada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,19 @@
     import bgVideoFdb from '$lib/videos/bg-video-fdb.mp4';
 </script>
 
-<div class="hero min-h-screen">
+<div class="min-h-screen flex">
     <video autoplay muted loop playsinline
-           class="max-h-full max-w-full h-full w-full object-cover -z-50">
+           class="h-screen w-screen object-cover absolute -z-50">
         <source src={bgVideoFdb} type="video/mp4" />
     </video>
-    <div class="hero-overlay bg-opacity-60">
-    </div>
-    <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
-            <h1 class="mb-5 text-5xl xl:text-8xl font-bold">Bienvenido a casa</h1>
-            <p class="mb-5">Porque en ti está la fuente de la vida; en tu luz vemos la luz. SALMO 36:9</p>
-            <button class="btn btn-primary">Get Started</button>
+    <div class="flex items-center px-4 pt-[66px] w-full">
+        <div class="sm:max-w-[100%] md:max-w-[90%] lg:max-w-[80%] xl:max-w-[70%] text-white">
+            <h1 class="mb-5 text-5xl md:text-6xl lg:text-8xl font-bold">Porque en ti está la fuente de la vida</h1>
+            <p class="mb-5 text-2xl md:text-3xl">en tu luz vemos la luz. SALMO 36:9</p>
+            <div class="flex flex-wrap gap-2">
+                <button class="bg-black rounded-lg p-4">¿Qué es Fuente de Bendiciones?</button>
+                <button class="bg-black rounded-lg p-4">¿Dónde puedo asistir?</button>
+            </div>
         </div>
     </div>
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,41 @@
-<script></script>
+<script>
+    import fdbLogo from '$lib/images/fdb-logo.png';
+    import cmnLogo from '$lib/images/cmn-logo.svg';
+    import {page} from '$app/stores';
+    import {afterNavigate} from "$app/navigation";
 
-<div class="absolute w-full">
-    <div class="navbar bg-white">
+    let logoActual;
+    switch ($page.url.pathname) {
+        case '/cmn':
+            logoActual = cmnLogo;
+            break;
+        default:
+            logoActual = fdbLogo;
+            break;
+    }
+    afterNavigate((navigation) => {
+        switch (navigation.to.url.pathname) {
+            case '/cmn':
+                logoActual = cmnLogo;
+                break;
+            default:
+                logoActual = fdbLogo;
+                break;
+        }
+    });
+
+    $: logo = logoActual;
+</script>
+
+<div class="absolute w-full bg-black">
+    <div class="navbar container m-auto">
         <div class="navbar-start">
-            <a href="/"
-               class="btn btn-ghost normal-case text-xl">
-                Fuente de Bendiciones
-            </a>
+            <img src="{logo}" alt="Fuente de bendiciones logo"
+                 class="w-[50px] h-auto">
         </div>
         <div class="navbar-end">
-            <a href="/cmn" class="btn btn-ghost normal-case font-normal">Comuna</a>
+            <a href="/" class="btn btn-ghost text-lg text-white normal-case font-normal">Inicio</a>
+            <a href="/cmn" class="btn btn-ghost text-lg text-white normal-case font-normal">Comuna</a>
         </div>
     </div>
 </div>
