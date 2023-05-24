8141c062f1f2d8fe4576aa3fd0646527ec3668c6
David J Chavez <davidjchavez98@gmail.com>
2023-05-24 14:23:55 -0600

    feat(hero): Pantalla principal finalizada

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from './Header.svelte';
+    import "../app.css";
 </script>
 
 <div>
@@ -9,43 +10,3 @@
         <slot/>
     </main>
 </div>
-
-<style>
-    main {
-        margin: 0 auto;
-        padding: 0 1rem;
-        transition-property: width, max-width;
-        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-        transition-duration: 150ms;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: 640px;
-        }
-    }
-
-    @media (min-width: 768px) {
-        main {
-            max-width: 768px;
-        }
-    }
-
-    @media (min-width: 1024px) {
-        main {
-            max-width: 1024px;
-        }
-    }
-
-    @media (min-width: 1280px) {
-        main {
-            max-width: 1280px;
-        }
-    }
-
-    @media (min-width: 1536px) {
-        main {
-            max-width: 1536px;
-        }
-    }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Inicio</h1>
-<img src="">
+<script>
+    import bgVideoFdb from '$lib/videos/bg-video-fdb.mp4';
+</script>
+
+<div class="hero min-h-screen">
+    <video autoplay muted loop playsinline
+           class="max-h-full max-w-full h-full w-full object-cover -z-50">
+        <source src={bgVideoFdb} type="video/mp4" />
+    </video>
+    <div class="hero-overlay bg-opacity-60">
+    </div>
+    <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl xl:text-8xl font-bold">Bienvenido a casa</h1>
+            <p class="mb-5">Porque en ti está la fuente de la vida; en tu luz vemos la luz. SALMO 36:9</p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
