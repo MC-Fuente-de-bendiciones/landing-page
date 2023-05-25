f47b7467cbfb921566cf062e55ad7d8ae4a709f5
David Josue Rodríguez Chávez <davidjchavez98@gmail.com>
2023-05-25 16:56:00 -0600

    feat(cmn): Hero de Convicciones finalizado

--- src/routes/cmn/+page.svelte
+++ src/routes/cmn/+page.svelte
@@ -1,25 +1,49 @@
 <script>
     import conviccionesVideo from '$lib/videos/convicciones.mp4';
-    import conviccionesPoster from '$lib/images/cmn/convicciones-poster.jpeg';
+    import conviccionesPostal from '$lib/images/cmn/convicciones-poster.jpeg';
+    import {fade, fly} from 'svelte/transition';
 
-    const showPoster = (event) => {
-        event.target.autoplay = false;
-        event.target.load();
+    export let videoFinalizado = false;
+
+    const toggleMuted = (event) => {
+        event.target.muted = !event.target.muted;
     };
 
+    const onEnded = () => {
+        videoFinalizado = true;
+    };
+
+    $: videoFinalizado;
 </script>
 
-<section id="hero" class="h-section flex justify-center py-12 px-4">
-    <video
-            on:ended={showPoster}
-            poster="{conviccionesPoster}"
-            autoplay
-            muted
-            playsinline
-            controls="controls"
-            class="w-full h-fit md:w-4/5 xl:w-1/2">
-        <source src="{conviccionesVideo}"/>
-    </video>
+<section id="hero" class="h-section relative flex justify-center items-center px-4">
+    {#if !videoFinalizado}
+        <div class="w-full h-fit absolute md:w-4/5 lg:w-1/2 xl:w-4/5 p-4">
+            <video on:ended={onEnded}
+                   on:click={toggleMuted}
+                   in:fade={{duration: 1500}}
+                   out:fly="{{y: 100, duration: 200}}"
+                   autoplay
+                   playsinline
+                   class="w-full rounded-2xl">
+                <source src="{conviccionesVideo}"/>
+            </video>
+        </div>
+    {:else}
+        <div class="flex flex-col gap-8 w-limit text-center">
+            <span in:fly="{{y: -50, duration: 500, delay: 500}}"
+                  class="portrait:text-[5vh] landscape:text-[4vw] text-white font-dela-gothic-one uppercase">
+                Congreso juvenil
+            </span>
+            <span in:fly="{{y: -50, duration: 500, delay: 1000}}"
+                  class="portrait:text-[4vh] landscape:text-[4vw] text-yellow-300 font-dela-gothic-one uppercase">
+                Convicciones 2.0
+            </span>
+            <picture in:fly="{{y: -50, duration: 500, delay: 1500}}" class="m-auto w-4/5">
+                <img src="{conviccionesPostal}" alt="conviccionesPostal" class="rounded-lg shadow-2xl">
+            </picture>
+        </div>
+    {/if}
 </section>
 
 
@@ -27,5 +51,9 @@
     #hero {
         background: url('/src/lib/backgrounds/layered-waves-cmn.svg') no-repeat center center;
         background-size: cover;
+    }
+
+    .w-limit {
+        @apply max-w-[100%] md:max-w-[90%] lg:max-w-[80%] xl:max-w-[70%];
     }
 </style>
